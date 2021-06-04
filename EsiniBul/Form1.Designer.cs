cebdef67f422df75c3972d397da1c5ddfd6c9e9b
ahmet güldaş <ahmet.guldas@hotmail.com>
2021-06-04 12:26:29 +0300

    Add project files.

---
+++ EsiniBul/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace EsiniBul
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlKartlar = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlKartlar
+            // 
+            this.pnlKartlar.BackColor = System.Drawing.Color.Gainsboro;
+            this.pnlKartlar.Location = new System.Drawing.Point(10, 10);
+            this.pnlKartlar.Name = "pnlKartlar";
+            this.pnlKartlar.Size = new System.Drawing.Size(1000, 1000);
+            this.pnlKartlar.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 1025);
+            this.Controls.Add(this.pnlKartlar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Esini Bul";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlKartlar;
+    }
+}
+
